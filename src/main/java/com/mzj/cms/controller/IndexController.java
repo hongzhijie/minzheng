b424f15e62a6e67b8a845901c79e44839d2894e0
hongzhijie <hzj_777@163.com>
2019-03-21 16:08:48 +0800

    上传文件代码提交

--- src/main/java/com/mzj/cms/controller/IndexController.java
+++ src/main/java/com/mzj/cms/controller/IndexController.java
@@ -36,10 +36,19 @@ import com.mzj.cms.service.DataCleaningService;
 import com.mzj.cms.architect.constant.Constants;
 import com.mzj.cms.handler.RedisClient;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 
 /**
@@ -56,6 +65,49 @@ public class IndexController extends BasicController {
     private DataCleaningService dataCleaningService;
     @Autowired
     private RedisClient redisClient;
+
+    // 文件上传路径
+    @Value("${spring.http.multipart.location}")
+    private String filePath;
+
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+    //测试上传文件请求
+    @RequestMapping(value = "upload")
+    @ResponseBody
+    public String upload(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return "文件为空";
+        }
+
+        // 获取文件名
+        String fileName = file.getOriginalFilename();
+        logger.info("上传的文件名为：" + fileName);
+
+        // 获取文件的后缀名
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));
+        logger.info("上传的后缀名为：" + suffixName);
+
+        // 解决中文问题，liunx下中文路径，图片显示问题
+        fileName = UUID.randomUUID() + suffixName;
+
+        File dest = new File(filePath + fileName);
+
+        // 检测是否存在目录
+        if (!dest.getParentFile().exists()) {
+            dest.getParentFile().mkdirs();
+        }
+
+        try {
+            file.transferTo(dest);
+            return "上传成功";
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "上传失败";
+    }
 
     /**
      *跳转到主页
